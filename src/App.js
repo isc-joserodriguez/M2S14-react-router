4d8789a047a365d72e1de8c321c9f7971d10de1e
José Rodriguez <isc.joserodriguez@gmail.com>
2021-10-23 09:39:02 -0600

    Ejemplo react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { useState } from 'react'
 import { Navbar, Container, NavDropdown, Nav } from 'react-bootstrap'
 import Home from './components/Home/Home'
+
+import { pacientes } from './utils/data';
 
 //Rutas 
 import { Switch, Route, Link } from 'react-router-dom'
 
 export default function App() {
+  const [pacientesArray, setPacientes] = useState(pacientes);
   return (
     <div>
       <ul>
@@ -12,9 +16,15 @@ export default function App() {
         <li><Link to='/about'>About</Link></li>
       </ul>
 
+      {pacientesArray.map((paciente, i) => <li key={i}>{paciente.nombre}</li>)}
+
       <Switch>
-        <Route path='/' component={Home} exact/>
+        <Route path='/' component={Home} exact />
         <Route path='/about' render={() => <div>About </div>} exact />
+        <Route path='/about' render={() => <Home prop1='' />} exact />
+        <Route>
+          <Home prop1='' prop2='' />
+        </Route>
       </Switch>
 
     </div>
