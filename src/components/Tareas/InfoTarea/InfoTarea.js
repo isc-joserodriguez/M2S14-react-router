5aa0d13df57c128d841d803da0fa20818ee0e8b4
José Rodriguez <isc.joserodriguez@gmail.com>
2021-10-30 13:11:56 -0600

    Ejercicio 2

--- src/components/Tareas/InfoTarea/InfoTarea.js
+++ src/components/Tareas/InfoTarea/InfoTarea.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import { getTarea } from '../../../services/tareas';
 
 const InfoTarea = () => {
     const { id } = useParams();
+    const [dataTarea, setDataTarea] = useState({
+        title: '',
+        description: ''
+    })
+
+    const getTareasNew = async () => {
+        const respuesta = await getTarea(id);
+        setDataTarea(respuesta)
+    }
+
+    useEffect(() => {
+        getTareasNew();
+    }, [])
+
+
     return (
         <div>
             {id}
+            <br />
+            {dataTarea.title}
+            <br />
+            {dataTarea.description}
+            <br />
             <br />
             <br />
             <br />
