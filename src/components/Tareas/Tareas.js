b1425327615744a592b0008bfae7323a2cadfde5
José Rodriguez <isc.joserodriguez@gmail.com>
2021-10-30 11:31:42 -0600

    Dependencias useEffect

--- src/components/Tareas/Tareas.js
+++ src/components/Tareas/Tareas.js
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import { getTareas } from '../../services/tareas'
+import axios from 'axios'
 
-const Tareas = () => {
+const Tareas = ({ contador = 0 }) => {
     const [listaTareas, setListaTareas] = useState([])
+    const [nombre, setNombre] = useState('pikachu');
+    const [dataPokemon, setDataPokemon] = useState({
+        name: 'NA',
+        order: 'NA'
+    });
 
     const getTareasNew = async () => {
         const respuesta = await getTareas();
@@ -12,14 +18,40 @@ const Tareas = () => {
     useEffect(() => {
         getTareasNew();
         console.log('montaje')
-    }, [])
+    }, [contador])
+
+    useEffect(() => {
+        let timeout = setTimeout(() => {
+            axios.get('https://pokeapi.co/api/v2/pokemon/' + nombre).then(resp => {
+                setDataPokemon({
+                    name: resp.data.name,
+                    order: resp.data.order
+                })
+            }).catch(err => {
+                console.log(err)
+            })
+        }, 1000);
+
+    }, [nombre])
 
     return (
-        <ul>
-            {listaTareas.map((tarea, i) => (
-                <li key={i}>{tarea.title} {tarea.description}</li>
-            ))}
-        </ul>
+        <div className='hola'>
+            <React.Fragment>
+                <ul style={{
+                    backgroundColor: 'red',
+                    fontSize: '50px'
+                }}>
+                    {listaTareas.map((tarea, i) => (
+                        <li key={i}>{tarea.title} {tarea.description}</li>
+                    ))}
+                </ul>
+
+                <input value={nombre} onChange={(e) => setNombre(e.target.value)} />
+                <br /><br />
+                {dataPokemon.name}<br />
+                {dataPokemon.order}<br />
+            </React.Fragment>
+        </div>
     )
 }
 
